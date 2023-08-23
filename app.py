2c78c81b85e9b82b9b12abd61a54f2ab1c078868
xuly94 <132195346+xuly94@users.noreply.github.com>
2023-08-23 20:16:38 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import lightgbm as lgb
-import pandas as pd
 import streamlit as st
 import numpy as np
 
@@ -17,21 +16,32 @@ prediction_type = st.radio("选择预测类型", ("AKD", "CKD"))
 
 # 根据预测类型指定特征列
 if prediction_type == "AKD":
-    feature_columns = ['Operativeduration','Hb','Bloodloss','Urine_protein','AKIGrade','Hct','ALB','SBP','BaslineeGFR','Anion_gap']
+    feature_columns = ['Operativeduration', 'Hb', 'Bloodloss', 'Urine_protein', 'AKIGrade', 'Hct', 'ALB', 'SBP', 'BaslineeGFR', 'Anion_gap']
 else:
-    feature_columns = ['BaslineeGFR','Age','Pathology','AKI_and_AKD','TBIL','Mg','Scr','TG','WBC','Urine_protein']
+    feature_columns = ['BaslineeGFR', 'Age', 'Pathology', 'AKI_and_AKD', 'TBIL', 'Mg', 'Scr', 'TG', 'WBC', 'Urine_protein']
 
-# 添加特征输入小部件
-input_features = []
-
+# 收集输入特征值
+input_features = {}
 for column in feature_columns:
-    feature_value = st.number_input(column, value=0.0)
-    input_features.append(feature_value)
+    if column == 'Urine_protein':
+        feature_value = st.selectbox(column, [0, 1, 2, 3])
+    elif column == 'AKI_and_AKD':
+        feature_value = st.selectbox(column, [0, 1, 2, 3])
+    elif column == 'Pathology':
+        feature_value = st.selectbox(column, [0, 1, 2])
+    elif column == 'Age':
+        feature_value = st.number_input(column, value=0, step=1)
+    else:
+        if column in ['Operativeduration', 'Hb', 'Bloodloss', 'Hct', 'SBP', 'ALB', 'BaslineeGFR', 'Anion_gap']:
+            feature_value = st.number_input(column, value=0.0, format="%.2f")
+        else:
+            feature_value = st.number_input(column, value=0, step=1)
+    input_features[column] = feature_value
 
 # 创建一个按钮，当用户点击时进行预测
 if st.button("预测"):
     # 将输入的特征值转换为数组
-    features = np.array([input_features])
+    features = np.array([[input_features[column] for column in feature_columns]])
 
     if prediction_type == "AKD":
         # 使用 AKD 模型进行预测
@@ -39,6 +49,5 @@ if st.button("预测"):
     else:
         # 使用 CKD 模型进行预测
         prediction = ckd_model.predict(features)
-
-    # 显示预测结果
-    st.write(f"预测结果: {prediction[0]}")
+    
+    st.write(f"预测结果: {prediction[1]}")
