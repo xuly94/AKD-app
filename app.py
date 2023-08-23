053cea7e02c83d367f13c08dac4280e3cd68b8f5
xuly94 <132195346+xuly94@users.noreply.github.com>
2023-08-23 16:31:38 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -3,16 +3,23 @@ import pandas as pd
 import streamlit as st
 import numpy as np
 
+# 加载AKD Prediction Model的LightGBM模型
+akd_model = lgb.Booster(model_file='model.txt')
 
-# 加载训练好的LightGBM模型
-model = lgb.Booster(model_file='model.txt')
+# 加载CKD Prediction Model的LightGBM模型
+ckd_model = lgb.Booster(model_file='ckd_model.txt')
 
 # 添加应用程序标题
-st.title("AKD Prediction Model for Nephrectomy")
+st.title("Prediction Model for Nephrectomy")
 
-# 读取特征列名（假设您有一个存储特征列名的文件或通过其他方式获取）
-# 在这里我将特征列名直接写在代码中，您可以根据需要进行修改
-feature_columns = ['Operativeduration','Hb','Bloodloss','Urine_protein','AKIGrade','Hct','ALB','SBP','BaslineeGFR','Anion_gap']
+# 用户选择预测 AKD 还是 CKD
+prediction_type = st.radio("选择预测类型", ("AKD", "CKD"))
+
+# 根据预测类型指定特征列
+if prediction_type == "AKD":
+    feature_columns = ['Operativeduration','Hb','Bloodloss','Urine_protein','AKIGrade','Hct','ALB','SBP','BaslineeGFR','Anion_gap']
+else:
+    feature_columns = ['BaslineeGFR','Age','Pathology','AKI_and_AKD','TBIL','Mg','Scr','TG','WBC','Urine_protein']
 
 # 添加特征输入小部件
 input_features = []
@@ -26,8 +33,12 @@ if st.button("预测"):
     # 将输入的特征值转换为数组
     features = np.array([input_features])
 
-    # 使用模型进行预测
-    prediction = model.predict(features)
+    if prediction_type == "AKD":
+        # 使用 AKD 模型进行预测
+        prediction = akd_model.predict(features)
+    else:
+        # 使用 CKD 模型进行预测
+        prediction = ckd_model.predict(features)
 
     # 显示预测结果
     st.write(f"预测结果: {prediction[0]}")
